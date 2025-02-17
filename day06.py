cdbda80da3a5ef2ae44b9d827a2eeafd48d86486
cholin3721 <cholin3721@gmail.com>
2025-02-17 09:42:45 +0900

    v0.2 bit operation

--- day06.py
+++ day06.py
@@ -7,3 +7,12 @@ def is_Even(n) -> bool:
         return True
     else :
         return False
+
+a = 10
+b = 11
+# bit operation
+
+print(a & b)
+
+n = int(input("정수를 입력하시오 : "))
+print(is_Even(n))
