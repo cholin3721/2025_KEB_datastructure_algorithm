7db927e3a8d674cb28420bce9ffb9f5489a1620c
cholin3721 <cholin3721@gmail.com>
2025-02-17 14:09:40 +0900

    v0.7 poly fixed

---
+++ poly.py
@@ -0,0 +1,35 @@
+def print_poly(f_x, t_x) -> str:
+    # term = len(f_x) - 1
+    poly_expression = "f(x) = "
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        term = t_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        # term = term - 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x, t_x) -> int:
+    return_value = 0
+    # term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, t_x[i])
+        # term = term - 1
+
+    return return_value
+
+
+# fx = [2, 3, 4, 0, -9]
+fx = [2,5,-9,11]
+tx =  [20, 7 , 2, 0]
+
+if __name__ == "__main__":
+    print(print_poly(fx, tx))
+    print(calculation_poly(int(input("x 값 : ")), fx, tx))
