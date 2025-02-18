1e1eeda648665d4a96fe61922c9487d6e5fd8f7d
cholin3721 <cholin3721@gmail.com>
2025-02-18 15:39:46 +0900

    v2.1 queue

--- day07.py
+++ day07.py
@@ -104,5 +104,6 @@ if __name__ == "__main__" :
     q.enqueue(-11)
     q.enqueue(8)
     print(q.size())
-    print(q.dequeue())
+    for i in range(q.size()):
+        print(q.dequeue())
     print(q.size())
