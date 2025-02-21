636788fb4769916d46df2eee051fdfcafaaf1b50
cholin3721 <cholin3721@gmail.com>
2025-02-21 10:23:25 +0900

    v3.3 bublle_sort

--- day10.py
+++ day10.py
@@ -1,8 +1,11 @@
 def bubble_sort(a_list):
     list_length = len(a_list)-1
     for i in range(list_length):
-        for j in range(list_length) :
+        for j in range(list_length -i) :
             if a_list[j] > a_list[j+1]:
                 a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
+                #print(j, end = ' ') 성능 측정용 print
+    return a_list
+
 
 print(bubble_sort([8, -11, 9, 1]))
