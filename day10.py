1c16523d029c491c9330819769a2c52f885e36d1
cholin3721 <cholin3721@gmail.com>
2025-02-21 11:21:27 +0900

    v3.5 insertion_sort

--- day10.py
+++ day10.py
@@ -1,3 +1,13 @@
+def insertion_sort(a_list):
+    for i in range(1, len(a_list)):
+        value = a_list[i]
+        while i > 0 and a_list[i-1] > value :
+            a_list[i] = a_list[i-1]
+            i = i-1
+        a_list[i] = value
+    return a_list
+
+
 def bubble_sort(a_list):
     list_length = len(a_list)-1
     for i in range(list_length):
@@ -12,4 +22,7 @@ def bubble_sort(a_list):
     return a_list
 
 
+
+
 print(bubble_sort([8, -11, 9, 1]))
+print(insertion_sort([8, -11, 9, 1]))
