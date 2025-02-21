90cd36ee223732eb91febf2a24b5d9460fc1556e
cholin3721 <cholin3721@gmail.com>
2025-02-21 10:27:42 +0900

    v3.4 bubble_sort

--- day10.py
+++ day10.py
@@ -1,10 +1,14 @@
 def bubble_sort(a_list):
     list_length = len(a_list)-1
     for i in range(list_length):
+        no_swaps = True
         for j in range(list_length -i) :
             if a_list[j] > a_list[j+1]:
                 a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
-                #print(j, end = ' ') 성능 측정용 print
+                no_swaps = False
+                #print(j, end = ' ') #성능 측정용 print
+        if no_swaps :
+            return a_list
     return a_list
 
 
